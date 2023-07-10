3c6292b99d67a1ed373a8c309273ed3c1484f574
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-07-10 23:42:55 +0000

    atualização/ dim_atores

--- models/marts/dim_ator.sql
+++ models/marts/dim_ator.sql
@@ -1,19 +1,18 @@
 with 
-    stg_discorama__atores as (
+    stg_atores as (
         select *
-        from {{ source('fivetran_discorama', 'actor') }} 
+        from {{ ref('stg_discorama__atores') }}
     )
 
- 
     , transformed as (
-        select 
+        select            
             id_ator
             , ator_primeiro_nome
             , ator_ultimo_nome
             , ator_nome_completo
             , ultima_atualizacao
             , el_fivetran
-        from stg_discorama__atores
+        from stg_atores
     )
 
 select *
