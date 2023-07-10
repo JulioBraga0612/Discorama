625d19d8af5b4cab12e969b20ea9efdba67cd81d
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-07-10 00:56:57 +0000

    address.csv

--- models/staging/stg_discorama__endereco.sql
+++ models/staging/stg_discorama__endereco.sql
@@ -2,14 +2,14 @@ with
     fonte_endereco as (
        select
             cast (address_id as int) as id_endereco
-            , (address) as endereco
+            , cast(address1 as string) as endereco
             , cast (city_id as int) as id_cidade
             , cast (district as string) as bairro
             , cast (phone as int) as telefone
             , cast (postal_code as int) as codigo_postal
             , cast (left(cast(last_update as string), 10) as date) as ultima_atualizacao 
             , cast (left(cast(_fivetran_synced as string), 10) as date) as el_fivetran
-        from {{ source('fivetran_discorama', 'address') }} 
+        from {{ref('address')}} 
     )
 select *
 from fonte_endereco
