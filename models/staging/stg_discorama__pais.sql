66cce2349527e788b117494244e3429786b0f69a
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-07-10 19:47:00 +0000

    atualização/stg_discorama__pais

--- models/staging/stg_discorama__pais.sql
+++ models/staging/stg_discorama__pais.sql
@@ -4,7 +4,7 @@ with
             cast (country_id as int) as id_pais
             , cast (country1 as string) as pais
             , cast (left(cast(last_update as string), 10) as date) as ultima_atualizacao 
-        from {{ref('city')}} 
+        from {{ref('country')}} 
     )
 select *
 from fonte_pais
