4e23e501db9732b9c6d212aaa6549720c6b098e8
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-07-10 21:05:45 +0000

    staging/stg_discorama__categoria_filme

--- models/staging/stg_discorama__ator_filme.sql
+++ models/staging/stg_discorama__ator_filme.sql
@@ -2,7 +2,7 @@ with
     fonte_ator_filme as (
        select
             cast(actor_id as int) as id_ator
-            , cast(film_id as int) as id_film
+            , cast(film_id as int) as id_filme
             , cast(left(cast(last_update as string), 10) as date) as ultima_atualizacao 
             , cast(left(cast(_fivetran_synced as string), 10) as date) as el_fivetran
         from {{ source('fivetran_discorama', 'film_actor') }} 
