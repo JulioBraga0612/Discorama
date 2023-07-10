e776e7537d65c958b3d310da9c485f1af71d379d
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-07-10 19:35:37 +0000

    alterações/ stg_discorama__cidade.sql

--- models/staging/stg_discorama__cidade.sql
+++ models/staging/stg_discorama__cidade.sql
@@ -2,7 +2,7 @@ with
     fonte_cidade as (
        select
             cast (city_id as int) as id_cidade
-            , cast (city as string) as cidade
+            , cast (city1 as string) as cidade
             , cast (country_id as int) as id_pais
             , cast (left(cast(last_update as string), 10) as date) as ultima_atualizacao 
         from {{ref('city')}} 
