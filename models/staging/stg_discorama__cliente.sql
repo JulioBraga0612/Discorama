5e97bbe65d55c86a4c23f066358270823183dcf0
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-07-10 20:06:44 +0000

    staging/stg_discorama__cliente

---
+++ models/staging/stg_discorama__cliente.sql
@@ -0,0 +1,18 @@
+with
+    fonte_cliente as (
+       select
+            cast (customer_id as int) as id_cliente
+            , cast(store_id as int) as id_loja
+            , cast (first_name as string) as cliente_primeiro_nome
+            , cast (last_name as string) as cliente_ultimo_nome
+            , first_name || ' ' || last_name as cliente_nome_completo
+            , cast (address_id as int) as id_endereco
+            , cast (activebool as string) as ativo_bool
+            , cast (left(cast(create_date as string), 10) as date) as ultima_atualizacao 
+            , cast (left(cast(last_update as string), 10) as date) as ultima_atualizacao 
+            , cast (left(cast(_fivetran_synced as string), 10) as date) as el_fivetran
+            , cast (active as int) as ativo
+        from {{ source('fivetran_discorama', 'customer') }} 
+    )
+select *
+from fonte_cliente
