1181e118bc943bd4399eb163bc2f6d3fb7bbfbb8
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-07-11 16:59:54 +0000

    atualização fact_alugueis

--- models/marts/fact_alugueis.sql
+++ models/marts/fact_alugueis.sql
@@ -1,27 +1,27 @@
 with
     dim_inventario as (
         select *
-        from {{ ref('') }}
+        from {{ ref('stg_discorama__inventario') }}
     )
 
     , dim_filmes as (
         select *
-        from {{ ref('') }}
+        from {{ ref('stg_discorama__filmes') }}
     )
 
     , dim_clientes as (
         select *
-        from {{ ref('') }}
+        from {{ ref('stg_discorama__clientes') }}
     )
 
     , dim_funcionarios as (
         select *
-        from {{ ref('') }}
+        from {{ ref('stg_discorama__funcionarios') }}
     )
 
     , int_alugueis as (
         select *
-        from {{ ref('') }}
+        from {{ ref('int_alugueis') }}
     )
 
     , joined as (
@@ -32,3 +32,6 @@ with
         left join dim_inventario on id_inventario = dim_inventario.id_inventario
         left join dim_filmes on dim_inventario.id_filme = dim_filmes.id_filme
     )
+
+select *
+from joined
