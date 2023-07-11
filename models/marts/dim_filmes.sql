9637fb656c180ec94e4617ce9dcefc91e28ee780
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-07-11 18:19:45 +0000

    alteração/dim_filmes

--- models/marts/dim_filmes.sql
+++ models/marts/dim_filmes.sql
@@ -14,8 +14,33 @@ with
         from {{ref('stg_discorama__categorias')}}
     )
 
-    , joined as (
-        select
-        from 
+    , stg_inventario as (
+        select *
+        from {{ref('stg_discorama__inventario')}}
     )
     
+    , joined as (
+        select 
+            stg_filmes.id_filme
+            , stg_inventario.id_inventario
+            , stg_inventario.id_loja
+            , stg_categorias.id_categoria
+            , stg_filmes.titulo_do_filme
+            , stg_filmes.descricao_do_filme
+            , stg_filmes.duracao_do_aluguel
+            , stg_filmes.avaliacao_do_aluguel
+            , stg_filmes.tamanho_do_aluguel
+            , stg_filmes.preco_substituicao
+            , stg_filmes.avaliacao
+            , stg_categoria_filme.id_categoria
+            , stg_categorias.categoria_nome
+
+
+        from  stg_filmes
+        left join stg_categoria_filme on stg_categoria_filme.id_filme = stg_filmes.id_filme
+        left join stg_inventario on stg_inventario.id_filme = stg_filmes.id_filme
+        left join stg_categorias on stg_categorias.id_categoria = stg_categoria_filme.id_categoria
+    )
+   
+   select *
+   from joined
